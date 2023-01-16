b764769f857485b7d42c7ba3f5a80d6dda20ba26
Nathan Moon <moonman700@gmail.com>
2023-01-16 13:13:58 -0500

    Added base Javascript functionality

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var X=0; X < topics.length; X++) {
+  console.log(topics[X])
+}
+
+
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
